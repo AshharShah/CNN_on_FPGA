281cc924aabf76652743508a72a70da9101009e0
khizaralishahcs <khizaralishahcs07@gmail.com>
2023-08-03 03:46:33 +0500

    jal and jalr done, verified

--- Khizar/single-cycle/maincontrol.v
+++ Khizar/single-cycle/maincontrol.v
@@ -1,4 +1,4 @@
-module maincontrol(opcode, branch, memread, memtoreg, aluop, memwrite, alusrc, regwrite);
+module maincontrol(opcode, branch, memread, memtoreg, aluop, memwrite, alusrc, regwrite, jal_select);
 
   //opcode types
   localparam I    = 7'b0010011;
@@ -6,30 +6,31 @@ module maincontrol(opcode, branch, memread, memtoreg, aluop, memwrite, alusrc, r
   localparam SB   = 7'b1100011;
   localparam I_LD = 7'b0000011;
   localparam R    = 7'b0110011;
-  //localparam J    = 7'b1101111;
+  localparam J    = 7'b1101111;
+  localparam J_R  = 7'b1100111;
 
   input [6:0] opcode;
 
-  output reg branch, memread, memtoreg, memwrite, alusrc, regwrite;
+  output reg branch, memread, memtoreg, memwrite, alusrc, regwrite, jal_select;
   output reg [1:0] aluop;
 
   always @ (opcode)
     begin
       case (opcode)
         R:
-          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 8'b10_0_0_1_0_0_0;
+          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch, jal_select} <= 9'b10_0_0_1_0_0_0_0;
         I_LD:
-          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 8'b00_1_1_1_1_0_0;
+          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch, jal_select} <= 9'b00_1_1_1_1_0_0_0;
         S:
-          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 8'b00_1_0_0_0_1_0;
+          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch, jal_select} <= 9'b00_1_0_0_0_1_0_0;
         SB:
-          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 8'b01_0_0_0_0_0_1;
+          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch, jal_select} <= 9'b01_0_0_0_0_0_1_0;
         I: // for addi (i-type)
-          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 8'b00_1_0_1_0_0_0;
-        //J:
-        //  {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 8'b01_1_0_1_0_0_1;
+          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch, jal_select} <= 9'b00_1_0_1_0_0_0_0;
+        J, J_R:
+          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch, jal_select} <= 9'b01_1_0_1_0_0_1_1;
         default:
-          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 8'b00_0_0_0_0_0_0;
+          {aluop, alusrc, memtoreg, regwrite, memread, memwrite, branch, jal_select} <= 9'b00_0_0_0_0_0_0_0;
       endcase
     end
 
