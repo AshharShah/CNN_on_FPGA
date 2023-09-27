5d087507400d93969eca20d6c263ea4044ff3e6a
AshharShah <ashhar.shah786@gmail.com>
2023-09-27 12:17:40 +0500

    CNN-3 Basic Structure

---
+++ Ashhar/CNN_using_C/matrix.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct Matrix(
+    int rows;
+    int columns;
+    float **elements;
+)
+
+void Matrix_Init(Matrix* x, int r, int c){
+
+    for(int i = 0; i < x->r; i++){
+        for(int j = 0; j < x->c; j++){
+            
+        }
+    }
+}
