a3d4b7355aaa56185eb5c7bf15ce1d973a190212
AshharShah <ashhar.shah786@gmail.com>
2023-10-19 13:17:10 +0500

    Added Matrix Sum

--- Ashhar/CNN_using_C/main.c
+++ Ashhar/CNN_using_C/main.c
@@ -4,16 +4,50 @@
 extern void Matrix_Init(struct Matrix *x, int r, int c);
 extern void Num_Zeros(struct Matrix *x, int r, int c);
 extern void print_matrix(struct Matrix *x, int r, int c);
+extern void free_matrix(struct Matrix *x);
+extern struct Matrix add_matrices(struct Matrix *matrix1, struct Matrix *matrix2);
+// extern void multiply_matrices(Matrix *matrix1, Matrix *matrix2);
 
 int main()
 {
     struct Matrix kernel;
+    struct Matrix patch;
 
-    Matrix_Init(&kernel, 2, 2);
+    int rows = 3;
+    int cols = 3;
 
-    Num_Zeros(&kernel, 2, 2);
+    // create a kernel matrix with zero as elements
+    Matrix_Init(&kernel, rows, cols);
+    Num_Zeros(&kernel, rows, cols);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            kernel.elements[i][j] = i+j;
+        }
+    }
 
-    print_matrix(&kernel, 2, 2);
+    // create a patch matrix with the same dimensions as the kernel
+    Matrix_Init(&patch, rows, cols);
+    Num_Zeros(&patch, rows, cols);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            patch.elements[i][j] = i+j;
+        }
+    }
+
+    // print the kernel and patch matrices
+    printf("\nKernel Matrix: \n");
+    print_matrix(&kernel, rows, cols);
+    printf("\nPatch Matrix: \n");
+    print_matrix(&patch, rows, cols);
+
+    // program to test the sum of the matrices
+    struct Matrix result = add_matrices(&kernel, &patch);
+    printf("\n Sum Result Matrix: \n");
+    print_matrix(&result, rows, cols);
 
     return 0;
 }
--- Ashhar/CNN_using_C/matrix.c
+++ Ashhar/CNN_using_C/matrix.c
@@ -40,3 +40,42 @@ void print_matrix(struct Matrix *x, int r, int c)
         printf("\n");
     }
 }
+
+void free_matrix(struct Matrix *x) {
+  for (int i = 0; i < x->rows; i++) {
+    free(x->elements[i]);
+  }
+  free(x->elements);
+  free(x);
+}
+
+struct Matrix add_matrices(struct Matrix *matrix1, struct Matrix *matrix2) {
+  struct Matrix result;
+
+  Matrix_Init(&result, matrix1->rows, matrix1->columns);
+
+  for (int i = 0; i < matrix1->rows; i++) {
+    for (int j = 0; j < matrix1->columns; j++) {
+      result.elements[i][j] = matrix1->elements[i][j] + matrix2->elements[i][j];
+    }
+  }
+  return result;
+}
+
+// struct Matrix *multiply_matrices(struct Matrix *matrix1, struct Matrix *matrix2) {
+//   if (matrix1->columns != matrix2->rows) {
+//     printf("The number of columns in the first matrix must equal the number of rows in the second matrix to be multiplied.\n");
+//     return NULL;
+//   }
+
+//   struct Matrix result;
+//   Matrix_Init(&result, matrix1->rows, matrix2->columns);
+//   for (int i = 0; i < matrix1->rows; i++) {
+//     for (int j = 0; j < matrix2->columns; j++) {
+//       for (int k = 0; k < matrix1->columns; k++) {
+//         result->elements[i][j] += matrix1->elements[i][k] * matrix2->elements[k][j];
+//       }
+//     }
+//   }
+//   return result;
+// }
