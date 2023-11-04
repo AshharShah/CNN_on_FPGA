de781eef5d10b797a243b1703dcdd6a966f25af6
AshharShah <ashhar.shah786@gmail.com>
2023-11-04 16:35:56 +0500

    CNN-4 Added Image Read Methods

--- Ashhar/CNN_using_C/image.h
+++
@@ -1,6 +0,0 @@
-struct Image{
-  unsigned char *data;
-  int width;
-  int height;
-  int channels;
-};
--- Ashhar/CNN_using_C/main.c
+++ Ashhar/CNN_using_C/main.c
@@ -1,11 +1,18 @@
 #include "matrix.h"
 #include <stdio.h>
 
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
 extern void Matrix_Init(struct Matrix *x, int r, int c);
 extern void Num_Zeros(struct Matrix *x, int r, int c);
 extern void print_matrix(struct Matrix *x, int r, int c);
 extern void free_matrix(struct Matrix *x);
 extern struct Matrix add_matrices(struct Matrix *matrix1, struct Matrix *matrix2);
+extern struct Matrix multiply_matrices(struct Matrix *matrix1, struct Matrix *matrix2);
 extern struct Matrix multiply_matrices(struct Matrix *matrix1, struct Matrix *matrix2);
 
 int main()
@@ -53,6 +60,28 @@ int main()
     struct Matrix multiply_result = multiply_matrices(&kernel, &patch);
     printf("\n Multiplication Result Matrix: \n");
     print_matrix(&multiply_result, rows, cols);
+    printf("\n\n");
+
+    // Declare a 2D array to store the image data.
+    uint8_t image_array[640][480];
+    int height = 0;
+    int width = 0;
+    int channels = 0;
+
+    // Load the image data into the array.
+    unsigned char* image_data = stbi_load("/home/ashhar/Desktop/CNN_on_FPGA/Ashhar/CNN_using_C/mnist_png/training/0/6034.png", &width, &height, &channels, 0);
+
+    if (image_data == NULL) {
+        printf("\nError Opening The Image!\n");
+        // Handle error.
+        return 1;
+    }
+
+    memcpy(image_array, image_data, width * height * channels);
+
+    printf("Height Of Image: %d\n", height);
+    printf("Width Of Image: %d\n", width);
+    printf("Channels In Image: %d\n", channels);
 
     return 0;
 }
